78edfda5749f7e0a4e9f8cf3368b1cf0ffd22660
Aleksandra Nizio <ola.nizio@o2.pl>
2024-11-10 15:47:05 +0100

    Konstruktory

--- cwiczonka/src/main/java/com/cwiczenia/Animal.java
+++ cwiczonka/src/main/java/com/cwiczenia/Animal.java
@@ -2,6 +2,26 @@ package com.cwiczenia;
 
 public class Animal {
     private String breed;
+    private String mammal;
+
+    //Konstruktory
+    Animal(){
+        this.setMammal("Dog");
+    }
+
+    Animal(String m){
+        this.setMammal(m);
+    }
+
+    //Getter
+    public String getMammal(){
+        return mammal;
+    }
+
+    //Setter
+    public void setMammal(String m){
+        this.mammal = m;
+    }
 
     //Getter
     public String getBreed(){
--- cwiczonka/src/main/java/com/cwiczenia/Program.java
+++ cwiczonka/src/main/java/com/cwiczenia/Program.java
@@ -3,8 +3,10 @@ package com.cwiczenia;
 class Program{
     public static void main(String[] args){
         Animal a1 = new Animal();
+        Animal a2 = new Animal();
+        Animal a3 = new Animal("Cat");
         a1.setBreed("Border Collie");
-        System.out.println(a1.getBreed());
+        System.out.println("a1: " + a1.getBreed() + " a2: "+ a2.getMammal()+ " a3: " + a3.getMammal());
         a1.bark();
     }
 }
